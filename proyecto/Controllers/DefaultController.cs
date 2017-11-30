f1a3aaf6afbdf47c76b5ee3ae0206d3b000e5288
Mauricio Zambrana <mauricio.zambrana.g@gmail.com>
2017-11-29 22:41:23 -0600

    se agrego la opcion de contacto y rotativa para enviar pdf el html e impimir con window.print()

--- proyecto/Controllers/DefaultController.cs
+++ proyecto/Controllers/DefaultController.cs
@@ -1,8 +1,10 @@
 ﻿using Model;
 using proyecto.App_Start;
+using proyecto.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,5 +18,58 @@ namespace proyecto.Controllers
         {
             return View(usuario.Obtener(FrontOfficeStartUp.Usuariovisualizando(),true));
         }
+
+        public JsonResult EnviarCorreo (ContactoViewModels model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var _usuario = usuario.Obtener(FrontOfficeStartUp.Usuariovisualizando());
+
+                    var mail = new MailMessage();
+                    mail.From = new MailAddress(model.Correo, model.Nombre);
+                    mail.To.Add(_usuario.Email);
+                    mail.Subject = "Correo desde Contacto.";
+                    mail.IsBodyHtml = true;
+                    mail.Body = model.Mensaje;
+
+                    var SmtpServer = new SmtpClient("smtp.live.com"); //smtp.gmail.com
+                    SmtpServer.Port = 587;
+                    SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    SmtpServer.UseDefaultCredentials = false;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("mzambranag@hotmail.com", "xxx");
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mail);
+
+
+                }
+                catch (Exception e)
+                {
+                    rm.SetResponse(false,e.Message);
+                    return Json(rm);
+                    throw;
+                }
+
+                rm.SetResponse(true);
+                rm.function = "CerrarContacto();";
+            }
+
+
+            return Json(rm);
+        }
+
+        public ActionResult ExportAPDF()
+        {
+            return new Rotativa.MVC.ActionAsPdf("PDF");
+        }
+
+        public ActionResult PDF()
+        {
+            return View(usuario.Obtener(FrontOfficeStartUp.Usuariovisualizando(),true));
+        }
+
     }
 }
---
+++ proyecto/ViewModels/ContactoViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace proyecto.ViewModels
+{
+    public class ContactoViewModels
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        public string Mensaje { get; set; }
+    }
+
+}
